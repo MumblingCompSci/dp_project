a829936ff414a2c83cd44399cedb40becf006e1c
Garrett Quintero <garrett.quintero@gmail.com>
2018-10-29 20:48:19 -0600

    skeleton main code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
 public class Main {
 
+    private static int numCities;
+    private static int numMonths;
+    private static int[][] relocCosts;
+    private static int[][] opsCosts;
+
+    private static Node[][] minCosts;
+    private static String[] cityNames;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //TODO: import city data
+        importCityData("someString.....", opsCosts, numCities, numMonths);
+
+        //TODO: import relocation data
+        importRelocCosts("someString........", relocCosts);
+
+        //TODO: calculate costs table
+        calculateCosts(relocCosts, opsCosts, minCosts);
+        //TODO: generate "city path"
+
     }
+
+    private static void calculateCosts(int[][] relocationCosts, int[][] operationalCosts, Node[][] minCosts) {
+        //TODO: calculate table data
+    }
+
+    private static void importCityData(String txtFile, int[][] opsCosts, int numCities, int numMonths) {
+        //TODO: import numCities
+        //TODO: import numMonths
+
+        //TODO: import operational costs
+
+    }
+
+    private static void importRelocCosts(String txtFile, int[][] relocCosts) {
+        //TODO: import relocation costs
+    }
+
+    private class Node {
+        public Node(int minCost, int cityIndex) {
+            this.minCost = minCost;
+            this.parentCity = cityIndex;
+        }
+
+        public int minCost;
+        public int parentCity;
+    }
+
 }
